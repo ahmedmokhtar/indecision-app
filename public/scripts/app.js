3380fbaf2f1b6c1281b4f6215c5ebbd65254de9e
Ahmed Ibrahim <me@ahmedmokhtar.com>
2018-12-13 22:14:02 +0200

    3-12: conditional rendering in JSX.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,6 +5,12 @@
 
 console.log('App.js is running!');
 
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
+};
+
 // JSX
 var template = React.createElement(
   'div',
@@ -12,12 +18,17 @@ var template = React.createElement(
   React.createElement(
     'h1',
     null,
-    'Indecision App'
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    'p',
+    null,
+    app.subtitle
   ),
   React.createElement(
     'p',
     null,
-    'This is some info'
+    app.options.length > 0 ? 'Here are your options' : 'No options'
   ),
   React.createElement(
     'ol',
@@ -35,27 +46,41 @@ var template = React.createElement(
   )
 );
 
+var user = {
+  name: 'Ahmed',
+  age: 43,
+  location: 'Egypt'
+};
+
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      'p',
+      null,
+      'Location: ',
+      location
+    );
+  }
+}
+
 var templateTwo = React.createElement(
   'div',
   null,
   React.createElement(
     'h1',
     null,
-    'Ahmed Mokhtar'
+    user.name ? user.name : 'Anonymous'
   ),
-  React.createElement(
+  user.age && user.age >= 18 && React.createElement(
     'p',
     null,
-    'Age: 43'
+    'Age: ',
+    user.age
   ),
-  React.createElement(
-    'p',
-    null,
-    'Location: Cairo'
-  )
+  getLocation(user.location)
 );
 
 var appRoot = document.getElementById('app');
 
-// ReactDOM.render(template, appRoot);
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
+// ReactDOM.render(templateTwo, appRoot);
