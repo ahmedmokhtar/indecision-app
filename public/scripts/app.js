f486a48f915c6b64cae75e8282ab1d235a3baf65
Ahmed Ibrahim <me@ahmedmokhtar.com>
2018-12-14 06:29:02 +0200

    3-17: manual data binding <= rendering function.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -65,42 +65,49 @@ function getLocation(location) {
 
 var count = 0;
 var addOne = function addOne() {
-  console.log('addOne');
+  count++;
+  renderCounterApp();
 };
 var minusOne = function minusOne() {
-  console.log('minusOne');
+  count--;
+  renderCounterApp();
 };
 var reset = function reset() {
-  console.log('reset');
+  count = 0;
+  renderCounterApp();
 };
-
-var templateTwo = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    'Count: ',
-    count
-  ),
-  React.createElement(
-    'button',
-    { onClick: addOne },
-    '+1'
-  ),
-  React.createElement(
-    'button',
-    { onClick: minusOne },
-    '-1'
-  ),
-  React.createElement(
-    'button',
-    { onClick: reset },
-    'reset'
-  )
-);
 
 var appRoot = document.getElementById('app');
 
-// ReactDOM.render(template, appRoot);
-ReactDOM.render(templateTwo, appRoot);
+var renderCounterApp = function renderCounterApp() {
+  var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      'Count: ',
+      count
+    ),
+    React.createElement(
+      'button',
+      { onClick: addOne },
+      '+1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: minusOne },
+      '-1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: reset },
+      'reset'
+    )
+  );
+
+  // ReactDOM.render(template, appRoot);
+  ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
--- src/app.js
+++ src/app.js
@@ -36,25 +36,32 @@ function getLocation(location) {
 
 let count = 0;
 const addOne = () => {
-  console.log('addOne');
+  count++;
+  renderCounterApp();
 };
 const minusOne = () => {
-  console.log('minusOne');
+  count--;
+  renderCounterApp();
 };
 const reset = () => {
-  console.log('reset');
+  count = 0;
+  renderCounterApp();
 };
-
-const templateTwo = (
-  <div>
-    <h1>Count: {count}</h1>
-    <button onClick={addOne}>+1</button>
-    <button onClick={minusOne}>-1</button>
-    <button onClick={reset}>reset</button>
-  </div>
-);
 
 const appRoot = document.getElementById('app');
 
-// ReactDOM.render(template, appRoot);
-ReactDOM.render(templateTwo, appRoot);
+const renderCounterApp = () => {
+  const templateTwo = (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={addOne}>+1</button>
+      <button onClick={minusOne}>-1</button>
+      <button onClick={reset}>reset</button>
+    </div>
+  );
+
+  // ReactDOM.render(template, appRoot);
+  ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
