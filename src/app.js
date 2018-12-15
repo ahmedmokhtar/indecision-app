fd563bca2a5ba503a1a477abb35e9a039e53a542
Ahmed Ibrahim <me@ahmedmokhtar.com>
2018-12-15 16:26:49 +0200

    4-29: events & methods.

--- src/app.js
+++ src/app.js
@@ -30,19 +30,28 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+  handlePick() {
+    alert('handlePick');
+  }
+
   render() {
     return (
       <div>
-        <button>What should I do?</button>
+        <button onClick={this.handlePick}>What should I do?</button>
       </div>
     );
   }
 }
 
 class Options extends React.Component {
+  handleRemoveAll() {
+    alert('handleRemoveAll');
+  }
+
   render() {
     return (
       <div>
+        <button onClick={this.handleRemoveAll}>Remove All</button>
         {
           this.props.options.map((option) => <Option key={option} optionText={option} />)
         }
@@ -62,10 +71,23 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+  handleAddOption(e) {    
+    e.preventDefault();
+    // e.persist();      // This is used to be able to see the real event object in the console.
+    const option = e.target.elements.option.value.trim();
+
+    if (option) {
+      alert(`handleAddOption: ${option}`);
+    }
+  }
+
   render() {
     return (
       <div>
-        <p>AddOption component here</p>
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option" />
+          <button>Add Option</button>
+        </form>
       </div>
     );
   }
