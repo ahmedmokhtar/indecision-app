e2e2de706a6cedd22494d9c432e496ac3ecf940b
Ahmed Ibrahim <me@ahmedmokhtar.com>
2018-12-15 04:22:12 +0200

    4-26, 4-27: Creating React components + nesting components.

--- src/app.js
+++ src/app.js
@@ -1,75 +1,70 @@
 // babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
 // live-server public
 
-console.log('App.js is running!');
-
-const app = {
-  title: 'Indecision App',
-  subtitle: 'Put your life in the hands of a computer',
-  options: []
-};
-
-const appRoot = document.getElementById('app');
-
-const onFormSubmit = (e) => {
-  e.preventDefault();
-
-  const option = e.target.elements.option.value;
-  
-  if (option) {
-    app.options.push(option);
-    e.target.elements.option.value = '';
-    render();
+class IndecisionApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+      </div>
+    );
   }
-};
-
-const onRemoveAll = () => {
-  app.options = [];
-  render();
-};
-
-const onMakeDecision = () => {
-  const randomNum = Math.floor(Math.random() * app.options.length);
-  const option = app.options[randomNum];
-  alert(option);
-};
-
-const user = {
-  name: 'Ahmed',
-  age: 43,
-  location: 'Egypt'
-};
-
-function getLocation(location) {
-  if (location) {
-    return <p>Location: {location}</p>;
-  } 
 }
 
-const render = () => {
-  const template = (
-    <div>
-      <h1>{app.title}</h1>
-      {app.subtitle && <p>{app.subtitle}</p>}
-      <p>{app.options.length > 0 ? 'Here are your options' : 'No options' }</p>
-      <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
-      <button onClick={onRemoveAll}>Remove all</button>
-      <ol>
-        {
-          app.options.map((option) => {
-            return <li key={option}>{option}</li>
-          })
-        }
-      </ol>
-      <form onSubmit={onFormSubmit}>
-        <input type="text" name="option" />
-        <button>Add Option</button>
-      </form>
-    </div>
-  );
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Indecision</h1>
+        <h2>Put your lif in the hands of a computer</h2>
+      </div>
+    );
+  }
+}
 
-  ReactDOM.render(template, appRoot);
-};
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>What should I do?</button>
+      </div>
+    );
+  }
+}
 
-render();
+class Options extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Options component here</p>
+        <Option />
+      </div>
+    );
+  }
+}
 
+class Option extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Option component here</p>
+      </div>
+    );
+  }
+}
+
+class AddOption extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>AddOption component here</p>
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
