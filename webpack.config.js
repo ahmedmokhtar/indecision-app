680c8f9061dd3dc4863b88a970a236bc47856d51
Ahmed Ibrahim <me@ahmedmokhtar.com>
2018-12-22 08:41:13 +0200

    8-64: setting up Webpack with SCSS.

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,11 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
-      }
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
     ]
   },
   resolve: {
