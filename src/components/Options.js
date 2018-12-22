7d4ef0324417e683c4ec3ef17fd7d8dfd3b614e1
Ahmed Ibrahim <me@ahmedmokhtar.com>
2018-12-22 19:14:34 +0200

    8-69: styling the Options list widget.

--- src/components/Options.js
+++ src/components/Options.js
@@ -4,13 +4,16 @@ import Option from './Option';
 
 const Options = (props) => (
   <div>
-    <button 
-      className="button button--link"
-      onClick={props.handleDeleteOptions}
-      disabled={props.options.length === 0}>  
-      Remove All
-    </button>
-    {props.options.length === 0 && <p>Please add an option to get started!</p>}
+    <div className="widget-header">
+      <h3 className="widget-header__content">Your Options</h3>
+      <button 
+        className="button button--link"
+        onClick={props.handleDeleteOptions}
+        disabled={props.options.length === 0}>  
+        Remove All
+      </button>
+    </div>
+    {props.options.length === 0 && <p className="widget__message">Please add an option to get started!</p>}
     {
       props.options.map((option) => (
         <Option 
